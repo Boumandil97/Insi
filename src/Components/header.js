02c1103a67db53a796dcaa170e29eef6c0c04380
boumandilyoussef <youssef.boumandil@cloudlink.us>
2022-04-18 12:48:35 +0000

    changes

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,10 +1,29 @@
 import React from "react";
 import '../Stylesheets/header.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const header= ()=>{
 return(
-    <div>
-        
+    <div class='header'>
+        <div id="firstRow">
+            <div id="dropdownLists">
+                <button class='btn btn-secondary dropdown-toggle' id="dropdownMenuButton" type="button" data-toggle='dropdown' aria-haspopup="true" aria-expanded="false">Customer</button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+
+                </div>
+            </div>
+            <div id="searchbarAndLogos">
+
+            </div>
+        </div>
+        <div id="secondRow">
+
+        </div>
+        <div id="thirdRow">
+
+        </div>
     </div>
 );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import Loginn from "./Components/Login";
-import Navbar from './Components/nav-bar'
+import Loginn from "./Components/Login";
+// import Navbar from './Components/nav-bar'
+// import Header from './Components/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navbar/>
+    <Loginn/>
   </React.StrictMode>
 );
 
